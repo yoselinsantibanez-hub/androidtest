c9c878b527726ad8b3c0a180803cd82e6814e727
Yoselin Santibañez <yoselinsantibanez@gmail.com>
2025-08-31 17:19:53 -0400

    intento

---
+++ app/src/main/java/com/example/intento1/Activity2.java
@@ -0,0 +1,35 @@
+package com.example.intento1;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+
+    TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_2);
+
+        txtResultado = findViewById(R.id.txtResultado);
+
+        Intent intent = getIntent();
+        String datoRecibido = intent.getStringExtra("datoNombre");
+        txtResultado.setText("Hola, " + datoRecibido);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/intento1/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.intento1;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextNombre;
+    Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        editTextNombre = findViewById(R.id.editTextNombre);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarDatos();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void enviarDatos(){
+        String nombre = editTextNombre.getText().toString();
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        intent.putExtra("datoNombre", nombre);
+        startActivity(intent);
+    }
+}
